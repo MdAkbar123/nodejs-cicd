bbcc8f1a3d319a55be4e4302aa5b3fe060dcad1b
MdAkbar123 <iamakbar816@gmail.com>
2025-11-03 13:42:10 +0530

    change deploy5

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "node_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["157.50.138.169/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
