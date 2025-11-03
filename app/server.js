b519bb3a9575e11d599da47ac9b87e24ce753022
MdAkbar123 <iamakbar816@gmail.com>
2025-11-03 19:08:38 +0530

    change code1

--- app/server.js
+++ app/server.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(80, () => {
-  console.log("Server running on port 80");
+const PORT = process.env.PORT || 3000; // Change from 80 to 3000
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
